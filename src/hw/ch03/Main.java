686211d5d0cf6f8c0ddf18dc3fc8df22c156ccda
yew00ni <yewonim805@gmail.com>
2026-03-27 00:23:48 +0900

    3장 과제 완료

--- src/hw/ch03/Main.java
+++ src/hw/ch03/Main.java
@@ -2,11 +2,14 @@ package hw.ch03;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("학번 : 20240823 / 이름 : 임예원");
+        System.out.println();
+
         // [3-1] 기본 테스트
-        AbstractDisplay d1 = new CharDisplay('H'); // 다형성 : 부모 클래스 타입으로 자식 클래스 객체를 참조
-        AbstractDisplay d2 = new StringDisplay("Hello"); // 다형성 : 부모 클래스 타입으로 자식 클래스 객체를 참조
-        d1.display();
-        d2.display();
+        // AbstractDisplay d1 = new CharDisplay('H'); // 다형성 : 부모 클래스 타입으로 자식 클래스 객체를 참조
+        // AbstractDisplay d2 = new StringDisplay("Hello"); // 다형성 : 부모 클래스 타입으로 자식 클래스 객체를 참조
+        // d1.display();
+        // d2.display();
 
         // [3-2] Step 1 테스트 : 반복 횟수 제어
         // AbstractDisplay d1 = new CharDisplay('X', 3); // 3회
@@ -16,7 +19,7 @@ public class Main {
 
         // [3-3] Step 2 테스트 : 새로운 Display
         // NumberDisplay
-        // AbstractDisplay num = new NumberDisplay(42, 4);
-        // num.display();
+        AbstractDisplay num = new NumberDisplay(42, 4);
+        num.display();
     }
 }
--- src/practice/ch04/framework/Product.java
+++ src/practice/ch04/framework/Product.java
@@ -1,5 +1,5 @@
 package practice.ch04.framework;
 
 public abstract class Product { // 추상클래스
-    public abstract void use(); // 추상 메소드(상속받으면 항상 구현해줘야 함)
+    public abstract void use(); // 추상 메소드(상속받으면 항상 구현해줘야 함, api)
 }
