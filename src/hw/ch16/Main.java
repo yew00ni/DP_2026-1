2b362da72060c5738d9a7cd73a0dbc1c8808897a
yew00ni <yewonim805@gmail.com>
2026-05-13 00:22:57 +0900

    16장 과제 완료

--- src/hw/ch16/Main.java
+++ src/hw/ch16/Main.java
@@ -2,6 +2,9 @@ package hw.ch16;
 
 public class Main {
     static public void main(String[] args) {
+        System.out.println("학번 : 20240823 / 이름 : 임예원");
+        System.out.println();
+
         new LoginFrame("Mediator Sample");
     }
 }
