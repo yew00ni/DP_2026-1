87efd379897a0671785cb1ca5964e4eee873282d
yew00ni <yewonim805@gmail.com>
2026-05-24 04:07:58 +0900

    19장 과제 완료

--- src/hw/ch19/Main.java
+++ src/hw/ch19/Main.java
@@ -2,6 +2,9 @@ package hw.ch19;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("학번 : 20240823 / 이름 : 임예원");
+        System.out.println();
+
         SafeFrame frame = new SafeFrame("State Sample");
         while (true) {
             for (int hour = 0; hour < 24; hour++) {
